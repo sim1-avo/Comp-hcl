3ceec9c2f0345968acdebd3ed37dc32514a3d37c
sim1-avo <56638877+sim1-avo@users.noreply.github.com>
2020-10-11 18:09:07 +0200

    Lexer switch operatori relazionali e metodo installID

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,14 +1,16 @@
+import jdk.nashorn.internal.parser.Token;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class Lexer {
-    ArrayList<SymbolTable> Symboltable;
+    ArrayList<Token> Tokentable;
     ArrayList<String> keywords;
     FileReader fileR;
     Reader reader;
     File file;
     public Lexer(String filename) throws FileNotFoundException {
-        Symboltable = new ArrayList<SymbolTable>();
+        Tokentable = new ArrayList<Token>();
         keywords= new ArrayList<String>();
         keywords.add("if");
         keywords.add("else");
@@ -63,7 +65,7 @@ public class Lexer {
                         retrack(); //torna indietro di un carattere dato che siamo andati di un carattere in avanti
                         return installID(lessema);
                     }
-                    default: break;
+                default: break;
 
             }
 
@@ -125,43 +127,136 @@ public class Lexer {
                         lessema = c;
                         state=10;
                     } else {
-                        state= //impostarlo al prossimo case o pattern (switch) oppure riportarlo a zero in modo che ricominci dagli spazi
+                        state= 12; //impostarlo al prossimo case o pattern (switch) oppure riportarlo a zero in modo che ricominci dagli spazi
                     }
                     break;
                 case 10:
                     if(c == '-') {
                         lessema+=c;
                         state=11;
-                    } else {
-                        retract();
-                        state= 12; //Significa che non è l'assegnazione e può essere uno degli operatori relazionali
-                    }
+                    }if(c == '='){
+                    lessema+=c;
+                    return new Token("lessequal", lessema);
+
+                } else {
+<<<<<<< HEAD
+                    retrack();
+                    state= 12; //Significa che non è l'assegnazione e può essere uno degli operatori relazionali
+=======
+                    retracK();
+                    return new Token("less", lessema);
+                    //state= 12; //Significa che non è l'assegnazione e può essere uno degli operatori relazionali
+>>>>>>> Lexer switch operatori relazionali e metodo installID
+                }
                     break;
                 case 11:
                     if(c == '-') {
                         lessema+=c;
-                        return new Token('assign', lessema);
+                        return new Token("assign", lessema);
                     } else {
-                        return new Token('notDefined', lessema); //Perchè se arriva fin qui, significa che nel lessema abbiamo <- ma non esiste un lessema <- quindi da errore
+                        return new Token("notDefined", lessema); //Perchè se arriva fin qui, significa che nel lessema abbiamo <- ma non esiste un lessema <- quindi da errore
                     }
                     break;
 
-                case 12: //Da implementare
+                case 12:
+                    if(c == '>'){
+                        lessema=c;
+                        state= 13;
+                    }else{
+                        state= 14;
+                    }
+                    break;
 
+                case 13:
+                    if(c == '='){
+                        lessema+=c;
+                        return new Token("greaterequal", lessema);
+                    }else{
+                        return new Token("greater", lessema);
+                    }
+                    break;
+
+                case 14:
+                    if(c == '='){
+                        lessema=c;
+                        state=15;
+                    }else{
+                        state= 16;
+                    }
+                    break;
+
+                case 15:
+                    if(c=='='){
+                        lessema+=c;
+                        return new Token("equal", lessema);
+                    }else{
+                        return new Token("notDefined", lessema); //Non assegno = all'assegnazione poichè per l'assegnazione abbiamo usato <--
+                    }
+                    break;
+
+                case 16:
+                    if(c=='!'){
+                        lessema=c;
+                        state=17;
+                    }else{
+                        state=18;
+                    }
+                    break;
+
+                case 17:
+                    if(c == '='){
+                        lessema+=c;
+                        return new Token("notequal", lessema);
+                    }else{
+                        return new Token("notDefined", lessema);
+                    }
+                    break;
+            }
+
+            //Numeri
+            switch(state){
+                case 18:
+                    if(Character.isDigit(c)){
+                        state = 19;
+                        lessema = c;
+                    }else{
+
+                    }
+                    break;
+
+                case 19:
+                    if(Character.isDigit(c)){
+                        lessema+=c;
+                    }else{
+                        return new Token("Number", lessema);
+                    }
+                    break;
             }
 
 
+        }
+
+    }
+    private Token installID(String lessema){
+        Token token;
+        //Controllo se il lessema è una parola chiave
+        for(String kw:keywords){
+            if(lessema.equals(kw)){
+                return kw;
+            }else{
+                token =  new Token("ID", lessema);
+                Tokentable.add(token);
+                return token;
+            }
+        }
 
 
 
 
+        private void retrack(){
 
 
         }
 
     }
 
-
-
-
-}
